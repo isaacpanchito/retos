b7cd14c2daca3eb3ec7dc0b8a1af23dce6271847
Yoari <yoari.gomez06@gmail.com>
2023-02-16 16:25:30 -0600

    avance 1.0_Yoari
    avance 1.0_Yoari

--- Assets/script/triggerRespuesta.cs
+++ Assets/script/triggerRespuesta.cs
@@ -20,14 +20,59 @@ public class triggerRespuesta : MonoBehaviour
             case "1-3":
             porcentaje+=.333;
             break;
-            case "3-3":
-            porcentaje+=1;
+            case "1-4":
+            porcentaje+=.25;
+            break;
+            case "1-5":
+            porcentaje+=.2;
             break;
             case "1-8":
             porcentaje+=.125;
             break;
-            case "3-5":
-            porcentaje+=.6;
+            case "1-9":
+            porcentaje+=.111;
+            break;
+            case "2-5":
+            porcentaje+=.4;
+            break;
+            case "2-6":
+            porcentaje+=.333;
+            break;
+            case "2-8":
+            porcentaje+=.25;
+            break;
+            case "3-3":
+            porcentaje+=1;
+            break;
+            case "3-8":
+            porcentaje+=.375;
+            break;
+            case "3-12":
+            porcentaje+=.25;
+            break;
+            case "4-5":
+            porcentaje+=.8;
+            break;
+            case "4-6":
+            porcentaje+=.666;
+            break;
+            case "4-7":
+            porcentaje+=.57;
+            break;
+            case "6-6":
+            porcentaje+=1;
+            break;
+            case "6-8":
+            porcentaje+=.75;
+            break;
+            case "7-4":
+            porcentaje+=1.75;
+            break;
+            case "7-10":
+            porcentaje+=.7;
+            break;
+            case "10-8":
+            porcentaje+=1.25;
             break;
         }
     }
