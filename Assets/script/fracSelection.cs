ca3dbfa51aaee2de1602ed20a1993beacee698d5
isaacpanchito <isaacemj.26@gmail.com>
2023-02-19 18:58:06 -0600

    Modelados
    Models

--- Assets/script/fracSelection.cs
+++ Assets/script/fracSelection.cs
@@ -16,6 +16,8 @@ public class fracSelection : MonoBehaviour
     }
     void generate(){
         
+        Instantiate(objectsToInstantiate[0], new Vector3(-9f,1f,-.24f), pos.transform.rotation);
+        Instantiate(objectsToInstantiate[0], new Vector3(-8f,1f,-.24f), pos.transform.rotation);
         for(int i=0; i<8;i++){
             frac=Random.Range(0,objectsToInstantiate.Length);
             Instantiate(objectsToInstantiate[frac], new Vector3(-7f+i*.8f,1f,-.24f), pos.transform.rotation);
